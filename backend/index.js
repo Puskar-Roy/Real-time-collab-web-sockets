d9a1c47fc073634a55080639679c88d1ac2bf565
Puskar-Roy <puskarroy600@gmail.com>
2023-12-10 14:16:51 +0530

    feat : add real time collab

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require('cors');
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+
+const port = 5000;
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.json({ sucess: true });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server Starting In Port ${port}`);
+});
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", socket=>{
+    console.log(`User Connected - ${socket.id}`);
+
+    socket.on("send_message",data=>{
+        console.log(data);
+        socket.broadcast.emit("received_message",data)
+    })
+
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:5000");
+function App() {
+  // const [count, setCount] = useState("")
+  const [code, setCode] = useState("")
+  // const sendMsg = () => {
+  //   socket.emit("send_message", { "data": count })
+  //   console.log(count);
+  // };
+  useEffect(() => {
+    socket.on("received_message", data => {
+      setCode(data.data)
+    })
+  }, [socket])
+
+  useEffect(() => {
+    socket.emit("send_message", { "data": code })
+  }, [code])
+
+  return (
+    <>
+      <h1>Write Your Code</h1>
+      <div className='editor'>
+        <textarea placeholder='Write Code' value={code} onChange={e => setCode(e.target.value)} cols="50" rows="40"></textarea>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
