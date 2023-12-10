bdea9c86e73ded571cf6a0583bf3753c7e219670
Puskar-Roy <puskarroy600@gmail.com>
2023-12-10 14:27:54 +0530

    chore : origin

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-time-code-puskar.vercel.app",
     methods: ["GET", "POST"],
   },
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 import io from 'socket.io-client'
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://puskar-real.onrender.com");
 function App() {
   // const [count, setCount] = useState("")
   const [code, setCode] = useState("")
